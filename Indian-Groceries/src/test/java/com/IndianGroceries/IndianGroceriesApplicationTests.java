bd20d26f9a4d25e53d9046bb7bdee455357f911a
KeerthiVaddella <keerthiv.work@gmail.com>
2020-09-22 23:00:13 -0600

    First Commit

---
+++ Indian-Groceries/src/test/java/com/IndianGroceries/IndianGroceriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IndianGroceries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndianGroceriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
