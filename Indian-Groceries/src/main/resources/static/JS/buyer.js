97961f9566d8c9161b72073c353ee53ed38eb730
KeerthiVaddella <keerthiv.work@gmail.com>
2020-10-08 02:09:32 -0600

    Seventh Commit

--- Indian-Groceries/src/main/resources/static/JS/buyer.js
+++ Indian-Groceries/src/main/resources/static/JS/buyer.js
@@ -1,3 +1,34 @@
 /**
- * 
+ Buyer page controlling using angular
  */
+ 
+ let app =angular.module("buyerOps",[]);
+ 
+ 		//OnClick buttons
+ 		app.controller("displayControl",function($scope){
+ 			$scope.newBuyer=false;
+ 			$scope.allBuyers=false;
+ 			$scope.allOrders=false;
+ 			
+ 			//displays Buyer Form
+ 			$scope.addNewBuyer=function(){
+ 				console.log("entered addNewBuyer");
+ 				$scope.newBuyer=true;
+ 				$scope.allBuyers=false;
+ 				$scope.allOrders=false;			
+ 			};
+ 			
+ 			//displays List of Buyers
+ 			$scope.showBuyers=function(){
+ 				$scope.allBuyers=true;
+ 				$scope.newBuyer=false;
+ 				$scope.allOrders=false;
+ 			}
+ 			
+ 			//displays Previous Orders of a Buyer
+ 			$scope.getPrevOrders=function(){
+ 				$scope.allOrders=true;
+ 				$scope.newBuyer=false;
+ 				$scope.allBuyers=false;
+ 			}
+ 		});
